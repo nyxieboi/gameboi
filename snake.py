97fcb51b58750c6a77332456e1c23094ed168ed7
Nora Abu Shamleh <nora.abushamleh@skola.botkyrka.se>
2019-11-07 11:08:00 +0100

    Initial commit

---
+++ snake.py
@@ -0,0 +1,31 @@
+#fucking snake game ffs
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube (object) :
+    rows = 0
+    w = 0
+    def _init_(self, start, dirnx=1, dirny=0, color=(255,0,0)) :
+        pass
+    def moce (slef, dirnx, dirny) :
+        pass
+    def draw (self, surface, eyes=Flase) :
+        pass
+
+class snake(object) :
+    def _init_(self , color, pos):
+        pass
+    def move(self): 
+        pass
+    def reset(self,pos):
+        pass
+    def addCube(self):
+        pass
+    def draw(self, surface): 
+        pass
+
+    
